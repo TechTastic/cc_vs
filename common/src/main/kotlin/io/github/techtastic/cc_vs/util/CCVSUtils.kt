00021b57bd35a1a7a005fad289867e66aa5bb329
TechTastic <74630543+TechTastic@users.noreply.github.com>
2024-11-01 15:31:54 -0500

    Renamed `getRotationMatrix` to `getTransformationMatrix`
    Renamed `positionToWorld` to `transformPositionToWorld`
    
    Added `getConstraints` to `ShipAPI`
    
    Added accessor from SpaceEye for accessing constraints map
    
    Version Bump

--- common/src/main/kotlin/io/github/techtastic/cc_vs/util/CCVSUtils.kt
+++ common/src/main/kotlin/io/github/techtastic/cc_vs/util/CCVSUtils.kt
@@ -12,6 +12,7 @@ import org.joml.Quaterniondc
 import org.joml.Vector3d
 import org.joml.Vector3dc
 import org.valkyrienskies.core.api.ships.ServerShip
+import org.valkyrienskies.core.apigame.constraints.*
 import org.valkyrienskies.mod.common.getShipManagingPos
 
 object CCVSUtils {
@@ -22,27 +23,27 @@ object CCVSUtils {
         if (!PlatformUtils.isCommandOnly() || computer.family == ComputerFamily.COMMAND)
             computer.addAPI(ExtendedShipAPI(computer.apiEnvironment, ship, level))
         else
-            computer.addAPI(ShipAPI(ship))
+            computer.addAPI(ShipAPI(ship, level))
     }
 
-    fun vectorToTable(vec: Vector3dc): Map<String, Double> = mapOf(
-        Pair("x", vec.x()),
-        Pair("y", vec.y()),
-        Pair("z", vec.z())
+    fun Vector3dc.toLua() = mapOf(
+        Pair("x", this.x()),
+        Pair("y", this.y()),
+        Pair("z", this.z())
     )
 
-    fun quatToTable(q: Quaterniondc): Map<String, Double> = mapOf(
-        Pair("x", q.x()),
-        Pair("y", q.y()),
-        Pair("z", q.z()),
-        Pair("w", q.w())
+    fun Quaterniondc.toLua() = mapOf(
+        Pair("x", this.x()),
+        Pair("y", this.y()),
+        Pair("z", this.z()),
+        Pair("w", this.w())
     )
 
-    fun momentToTable(moment: Matrix3dc): List<List<Double>> {
+    fun Matrix3dc.toLua(): List<List<Double>> {
         val tensor: MutableList<List<Double>> = mutableListOf()
 
         for (i in 0..2) {
-            val row = moment.getRow(i, Vector3d())
+            val row = this.getRow(i, Vector3d())
             tensor.add(i, listOf(row.x, row.y, row.z))
         }
 
@@ -50,4 +51,74 @@ object CCVSUtils {
     }
 
     fun getShip(level: ServerLevel, pos: BlockPos) = level.getShipManagingPos(pos)
+
+    fun VSConstraintAndId.toLua() = mapOf(Pair("id", this.constraintId), Pair("constraint", this.vsConstraint.toLua()))
+
+    fun VSConstraint.toLua(): Map<String, Any> {
+        val constraint = mutableMapOf<String, Any>()
+
+        constraint["shipId0"] = this.shipId0
+        constraint["shipId1"] = this.shipId1
+        constraint["type"] = this.constraintType.name
+        constraint["compliance"] = this.compliance
+
+        if (this is VSForceConstraint) {
+            constraint["localPos0"] = this.localPos0.toLua()
+            constraint["localPos1"] = this.localPos1.toLua()
+            constraint["maxForce"] = this.maxForce
+        }
+
+        if (this is VSTorqueConstraint) {
+            constraint["localRot0"] = this.localRot0.toLua()
+            constraint["localRot1"] = this.localRot1.toLua()
+            constraint["maxTorque"] = this.maxTorque
+        }
+
+        when (this) {
+            is VSAttachmentConstraint -> {
+                constraint["fixedDistance"] = this.fixedDistance
+            }
+
+            is VSHingeSwingLimitsConstraint -> {
+                constraint["minSwingAngle"] = this.minSwingAngle
+                constraint["maxSwingAngle"] = this.maxSwingAngle
+            }
+
+            is VSHingeTargetAngleConstraint -> {
+                constraint["targetAngle"] = this.targetAngle
+                constraint["nextTickTargetAngle"] = this.nextTickTargetAngle
+            }
+
+            is VSPosDampingConstraint -> {
+                constraint["posDamping"] = this.posDamping
+            }
+
+            is VSRopeConstraint -> {
+                constraint["ropeLength"] = this.ropeLength
+            }
+
+            is VSRotDampingConstraint -> {
+                constraint["rotDamping"] = this.rotDamping
+                constraint["rotDampingAxes"] = this.rotDampingAxes.name
+            }
+
+            is VSSlideConstraint -> {
+                constraint["localSlideAxis0"] = this.localSlideAxis0
+                constraint["maxDistBetweenPoints"] = this.maxDistBetweenPoints
+            }
+
+            is VSSphericalSwingLimitsConstraint -> {
+                constraint["minSwingAngle"] = this.minSwingAngle
+                constraint["maxSwingAngle"] = this.maxSwingAngle
+            }
+
+            is VSSphericalTwistLimitsConstraint -> {
+                constraint["minTwistAngle"] = this.minTwistAngle
+                constraint["maxTwistAngle"] = this.maxTwistAngle
+            }
+            else -> {}
+        }
+
+        return constraint
+    }
 }
